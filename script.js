5d3706c8e79b1becc5770f767877bccba75268e3
jhnlee817 <yobjohn2@yahoo.com>
2022-12-13 12:53:02 -0600

    make responsive navbar

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = (event) => {
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
+
+}
