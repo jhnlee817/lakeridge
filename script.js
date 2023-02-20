09ac34fc741838f628e425f14dc41d9e632a945e
jhnlee817 <yobjohn2@yahoo.com>
2023-02-20 14:26:24 -0600

    make image carousel

--- script.js
+++ script.js
@@ -7,4 +7,19 @@ toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
 
+
+const images = ["images/stockbanner2.jpg","images/stockbanner3.jpg","images/stockbanner4.jpg"];
+const carousel = document.querySelector(".carousel");
+const interval = setInterval(function() {
+    startCarousel();
+}, 3000);
+var index = 1;
+
+startCarousel = () => {
+    carousel.style.backgroundImage = `url(${images[index++]})`;
+    carousel.classList.remove("fade");
+    void carousel.offsetWidth;
+    carousel.classList.add("fade");
+    if(index > images.length - 1) index = 0;
+};
 }
